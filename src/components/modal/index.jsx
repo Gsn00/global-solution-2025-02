cf46dcae6d2ac3d9112174ea0db42cff2d11bf15
Gabriel Novais <silvanovaesgabriel@gmail.com>
2025-11-16 00:42:30 -0300

    Adjusting modal padding and font size

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -2,7 +2,7 @@ import { createPortal } from "react-dom";
 
 export default function Modal({ children }) {
   return createPortal(
-    <div className="flex items-center justify-center fixed top-0 left-0 z-40 w-full h-screen bg-black/50 p-10">
+    <div className="flex items-center justify-center fixed top-0 left-0 z-40 w-full h-screen bg-black/50 py-10 px-3">
       {children}
     </div>,
     document.body
--- src/components/userlist/index.jsx
+++ src/components/userlist/index.jsx
@@ -6,7 +6,7 @@ import Pagination from "../pagination";
 export default function UserList({ openModal }) {
   return (
     <section className="flex flex-col gap-5 px-5">
-      <h1 className="font-bold text-5xl text-text-light-primary dark:text-text-dark-primary">
+      <h1 className="font-bold text-3xl min-[760px]:text-5xl text-text-light-primary dark:text-text-dark-primary">
         Explorar Talentos
       </h1>
       <p className="text-xl text-text-light-secondary dark:text-text-dark-secondary">
